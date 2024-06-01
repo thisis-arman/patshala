6709c57022648800c09e05dffbf6b3242022a818
Arman Hossen <armanhossen1549@gmail.com>
2024-06-01 21:41:32 +0600

    updated

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,4 +1,6 @@
+import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
 import { studentServices } from "./student.service";
 
 
@@ -6,5 +8,43 @@ import { studentServices } from "./student.service";
 const getSingleStudent =catchAsync( async (req,res,next) => {
     
     const result = await studentServices.getSingleStudentFromDB(req.params.studentId);
-    return result;
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Student is retrieved successfully",
+        data: result,
+    })
 })
+
+
+const deleteSingleStudent =catchAsync( async (req,res,next) => {
+    const result = await studentServices.deleteSingleStudentIntoDB(req.params.studentId);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Student is deleted successfully",
+        data: result,
+    })
+})
+
+
+
+const updateSingleStudent =catchAsync( async (req,res,next) => {
+    
+    const result = await studentServices.updateStudentIntoDB(req.params.studentId,req.body);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Student is retrieved successfully",
+        data: result,
+    })
+})
+
+export const studentControllers = {
+  getSingleStudent,
+  updateSingleStudent,
+  deleteSingleStudent,
+};
