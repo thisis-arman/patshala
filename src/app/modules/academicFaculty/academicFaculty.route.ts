e00245b0d741e2db2532d6f3914fe6d2ad2d38fb
Arman Hossen <armanhossen1549@gmail.com>
2024-06-02 01:06:49 +0600

    academic faculty routes added

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,10 +1,21 @@
-
-import express from 'express';
-import { academicFacultyController } from './academicFaculty.controller';
-import validateRequest from '../../middlewares/validateRequest';
-
+import express from "express";
+import { academicFacultyController } from "./academicFaculty.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { academicFacultyValidations } from "./academicFaculty.zod.validation";
 
 const router = express.Router();
 
+router.post(
+  "/create-academic-faculty",
+  validateRequest(
+    academicFacultyValidations.createAcademicFacultyValidationSchema
+  ),
+  academicFacultyController.createAcademicFaculty
+);
 
-router.post('/create-academic-faculty',validateRequest(),academicFacultyController.createAcademicFaculty)
+router.get("/:facultyId", academicFacultyController.getSingleAcademicFaculty);
+router.patch("/:facultyId", academicFacultyController.updateAcademicFaculty);
+router.patch("/:facultyId", academicFacultyController.deleteAcademicFaculty);
+
+
+export const AcademicFacultyRoute = router;
