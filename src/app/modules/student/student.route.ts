0f2ec24c354508c569ebb611f23a0427c30f9cad
Arman Hossen <armanhossen1549@gmail.com>
2024-06-02 01:07:25 +0600

    academic faculty routes added

---
+++ src/app/modules/student/student.route.ts
@@ -0,0 +1,27 @@
+import  express from "express";
+import validateRequest from "../../middlewares/validateRequest";
+import { studentControllers } from "./student.controller";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  studentControllers.getAllStudents
+);
+router.get(
+  "/:studentId",
+  studentControllers.getSingleStudent
+);
+
+router.patch(
+  "/:studentId",
+  studentControllers.updateSingleStudent
+);
+
+router.patch(
+  "/:studentId",
+  studentControllers.deleteSingleStudent
+);
+
+
+export const AcademicFacultyRoute = router;
