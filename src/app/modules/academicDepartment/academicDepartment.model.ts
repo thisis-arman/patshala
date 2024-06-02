e172e6f2a66bf06734bb9c82e24171cdfa311c22
Arman Hossen <armanhossen1549@gmail.com>
2024-06-02 23:35:06 +0600

    academic Department model updated

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,16 +1,23 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 import TAcademicDepartment from "./academicDepartment.interface";
 
+const createAcademicDepartmentSchema = new Schema<TAcademicDepartment>({
+  name: {
+    type: String,
+    required: [true, "Department name is Required"],
+    unique: true,
+  },
+  academicFaculty: {
+    type: Schema.Types.ObjectId,
+    required: [true, "Faculty is required"],
+  },
+});
 
+export const academicDepartmentSchemas = {
+  createAcademicDepartmentSchema,
+};
 
-const createAcademicDepartment = new Schema < TAcademicDepartment > ({
-    name: {
-        type: String,
-        required: [true, "Department name is Required"],
-        unique: true,
-    },
-    academicFaculty: {
-        type: Schema.Types.ObjectId,
-        required:[true,"Faculty is required"]
-    }
-})
+export const AcademicDepartment = model<TAcademicDepartment>(
+  "AcademicDepartment",
+  createAcademicDepartmentSchema
+);
