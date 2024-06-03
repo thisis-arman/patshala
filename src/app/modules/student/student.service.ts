9ea0764155f51d60dadd27f8998f7836ee0b13f8
Arman Hossen <armanhossen1549@gmail.com>
2024-06-04 00:14:33 +0600

    dynamic non-primitive data updated

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,31 +1,47 @@
 import { TStudent } from "./student.interface";
-import { Student } from "./student.modal"
-
+import { Student } from "./student.modal";
 
 const getAllStudentsFromDB = async () => {
-    const result = await Student.find({});
-    return result;
-}
+  const result = await Student.find({});
+  return result;
+};
 
-const getSingleStudentFromDB = async (_id :string) => {
-    const result = await Student.findOne({_id});
-    return result;
-}
+const getSingleStudentFromDB = async (_id: string) => {
+  const result = await Student.findOne({ _id });
+  return result;
+};
 
+const updateStudentIntoDB = async (_id: string, payload: Partial<TStudent>) => {
+  const { name, guardian, localGuardian, ...restOfStudentInfo } = payload;
 
-const updateStudentIntoDB = async (_id :string,payload:Partial<TStudent>) => {
-    const result = await Student.findByIdAndUpdate({_id},{payload});
-    return result;
-}
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...restOfStudentInfo,
+  };
 
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
+      modifiedUpdatedData[`guardian.${key}`] = value;
+    }
+  }
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`locaGuardian.${key}`] = value;
+    }
+  }
 
-const deleteSingleStudentIntoDB = async (_id :string) => {
+  const result = await Student.findByIdAndUpdate({ _id }, { payload });
+  return result;
+};
 
-    const result = await Student.findByIdAndUpdate({ _id }, { isDeleted: true })
-    return result;
-    
-}
-
+const deleteSingleStudentIntoDB = async (_id: string) => {
+  const result = await Student.findByIdAndUpdate({ _id }, { isDeleted: true });
+  return result;
+};
 
 export const studentServices = {
   getAllStudentsFromDB,
