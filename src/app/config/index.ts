d790339e57769783cd632016a494841f4f63efc4
Arman Hossen <armanhossen1549@gmail.com>
2024-06-14 23:48:00 +0600

    password hashed

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,8 +5,9 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-    NODE_ENV: process.env.NODE_ENV ,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    default_password:process.env.DEFAULT_PASSWORD
-}
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  default_password: process.env.DEFAULT_PASSWORD,
+  bcrypt_salt_rounds: process.env.SLAT_ROUND,
+};
--- src/app/modules/user/user.modal.ts
+++ src/app/modules/user/user.modal.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 export const userSchema = new Schema<TUser>(
   {
@@ -35,4 +37,19 @@ export const userSchema = new Schema<TUser>(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
+
+
+userSchema.post('save',  function (doc,next) { 
+  doc.password = '';
+  next()
+})
 export const User = model<TUser>("User", userSchema);
