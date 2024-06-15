98e7318f4b3a63ed05d17e1765d817b3c221f9d6
Arman Hossen <armanhossen1549@gmail.com>
2024-06-15 23:59:51 +0600

    updated

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import Auth from '../../middlewares/Auth';
 import { AuthController } from './auth.controller';
+import { USER_ROLE } from '../user/user.constant';
 
 
 const router = express.Router();
 
 
-router.post('/login',Auth(),AuthController.loginUser)
+router.post('/login',Auth(USER_ROLE.admin),AuthController.loginUser)
 
 
 export const AuthRoutes = router;
